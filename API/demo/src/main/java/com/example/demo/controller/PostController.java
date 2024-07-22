ef12a5b283586ad16ecd10883a05acb7374912b6
JonathanAmponsahBoadu <kkwesi956@gmail.com>
2024-07-22 03:13:08 +0000

    API was modified

---
+++ API/demo/src/main/java/com/example/demo/controller/PostController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Post;
+import com.example.demo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postRepository.save(post);
+    }
+}
